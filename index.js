526d6f1605a8979ae1a626545a4b519b19da4e1f
Tobias Trinks <tobias.trinks@code.berlin>
2018-01-10 15:09:52 +0100

    first version of testing environment

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+
+app.use('/', require('./src/middleware/accessControl').main);
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
 app.use('/', require('./src/routes'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
--- src/controller/order.js
+++ src/controller/order.js
@@ -1,6 +1,7 @@
 exports.sendOrder = function (req, res) {
     var menu = ["Wasser", "Cola", "Bier"];
-    var input = req.body.params.order;
+    var input = req.body.order;
+    console.log(input);
     var order = require("../library/orderAlgorithm").main(menu, input);
     res.send("Du hast " + order.join(" und ") + " bestellt.");
 };
