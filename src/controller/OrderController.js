66ed63b624a493d27926a94432aac1d4eee91f58
Henrik Engelbrink <hengel2810@gmail.com>
2018-02-04 10:28:37 +0100

    Fixed bug

--- src/controller/OrderController.js
+++ src/controller/OrderController.js
@@ -1,16 +1,16 @@
 'use strict';
 
 const APIController = require('./APIController');
-const RealmDrinkController = require('../../ro-realm/controller/RealmDrinkController');
+const RealmOrderController = require('../../ro-realm/controller/RealmOrderController');
 
 class OrderController extends APIController {
     constructor () {
         super();
-        this.realmController = new RealmDrinkController();
+        this.realmController = new RealmOrderController();
         this.getOrder = this.getOrder.bind(this);
     };
     getOrder (req, res) {
-        let reqValid = this.requestValidator.validRequestData(req.body, ['order']);
+        let reqValid = this.requestValidator.validRequestData(req.body, ['order', 'voiceDeviceId']);
         this.handleRequest(reqValid, () => {
             let input = req.body.order;
             // load menu from database
@@ -19,6 +19,10 @@ class OrderController extends APIController {
 
             let nlpAlgorithm = require('../library/nlpAlgorithm/main');
             let order = nlpAlgorithm(menu, input, req.originalUrl);
+
+            let voiceDeviceId = req.body.voiceDeviceId;
+             // ########
+             // TBD: Add new order to database
 
             return order;
         }, res);
