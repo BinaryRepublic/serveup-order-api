54631a987cba51b3e00b76a94d7603f9d5637a34
tobiastrinks <tobias.trinks@code.berlin>
2018-03-24 14:20:50 +0100

    allowed options preflight request

--- src/middleware/accessControl.js
+++ src/middleware/accessControl.js
@@ -1,13 +1,11 @@
 exports.main = function (req, res, next) {
-    // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
-    // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Access-Token');
     res.setHeader('Access-Control-Allow-Credentials', true);
-    // Pass to next layer of middleware
-    next();
+    if (req.method === 'OPTIONS') {
+        res.send(200);
+    } else {
+        next();
+    }
 };
